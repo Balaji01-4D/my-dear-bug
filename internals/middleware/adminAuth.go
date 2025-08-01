28374c0893de9c5bf658f8f4e4f2defe8acc318e
Balaji01-4D <j.balaji2468@gmail.com>
2025-08-01 14:39:17 +0530

    added middleware for admin controlls

---
+++ internals/middleware/adminAuth.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"os"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func AdminAuthMiddleware() gin.HandlerFunc {
+	username := os.Getenv("ADMIN_USERNAME")
+	password := os.Getenv("ADMIN_PASSWORD")
+
+	return func(c *gin.Context) {
+		user, pass, ok := c.Request.BasicAuth()
+		if !ok || user != username || pass != password {
+			c.Header("WWW-Authenticate", `Basic realm="Restricted"`)
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
