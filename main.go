a00636542924cfcca90a177c34892ab557c27a83
Balaji01-4D <j.balaji2468@gmail.com>
2025-08-08 22:15:09 +0530

    added tags register routes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Balaji01-4D/my-dear-bug/config"
 	bug "github.com/Balaji01-4D/my-dear-bug/internals/confession"
+	"github.com/Balaji01-4D/my-dear-bug/internals/tag"
 	"github.com/Balaji01-4D/my-dear-bug/internals/upvote"
 	"github.com/gin-gonic/gin"
 )
@@ -15,6 +16,7 @@ func main() {
 	r := gin.Default()
 	bug.RegisterRoutes(r, db)
 	upvote.RegisterRoutes(r, db)
+	tag.RegisterRoutes(r, db)
 
 	r.Run()
 }
